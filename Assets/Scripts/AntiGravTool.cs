ae3d64dd097b5de81b3c422c5603fb3435b9d26f
Pixel / IKStreamIvo <ivostreaming@gmail.com>
2016-12-19 11:26:37 +0100

    Better Player
    Now using Unity's standard assets for the player.

--- Assets/Scripts/AntiGravTool.cs
+++ Assets/Scripts/AntiGravTool.cs
@@ -4,22 +4,55 @@ using UnityEngine;
 
 public class AntiGravTool : Tool {
 
-	// Use this for initialization
-	void Start () {
-        if (transform.parent)
-            if (transform.parent.name.Contains("Hand"))
-                transform.parent.parent.GetComponent<PlayerRays>().pickingUp = true;
-    }
+    private bool _mouseState;
+    private GameObject target;
+    public Vector3 screenSpace;
+    public Vector3 offset;
 
-    void OnDestroy()
-    {
-        if (transform.parent)
-            if (transform.parent.name.Contains("Hand"))
-                transform.parent.parent.GetComponent<PlayerRays>().pickingUp = false;
+	public override void Update() {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Transform hit = base.castRay(10f);
+
+            // Check if there was actually a hit..
+            if (!hit)
+                return;
+            if (hit.parent)
+                if (hit.parent.GetComponent<ClickableObject>())
+                    hit = hit.parent;
+
+            if (hit.GetComponent<ClickableObject>())
+            {
+                if (hit.GetComponent<ClickableObject>().canPickedUp)
+                {
+                    // ANTIGRAV :D
+                    _mouseState = true;
+                    target = hit.gameObject;
+                    target.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+                    screenSpace = Camera.main.WorldToScreenPoint(target.transform.position);
+                    offset = target.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z));
+                }
+            }
+        }
+
+        // MORE ANTIGRAV
+        if (Input.GetMouseButtonUp(0))
+        {
+            _mouseState = false;
+            if(target)
+                target.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        }
+
+        if (_mouseState)
+        {
+            //keep track of the mouse position
+            var curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z);
+
+            //convert the screen mouse position to world point and adjust with offset
+            var curPosition = Camera.main.ScreenToWorldPoint(curScreenSpace) + offset;
+
+            //update the position of the object in the world
+            target.transform.position = curPosition;
+        }
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
--- Assets/Scripts/Objects/Tool.cs
+++ Assets/Scripts/Objects/Tool.cs
@@ -7,4 +7,55 @@ public class Tool : MonoBehaviour {
     public string toolType;
     // public float durability;
 
+    public virtual void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Transform hit = castRay(10f);
+
+            if (!hit)
+                return;
+            if (hit.parent)
+                if (hit.parent.GetComponent<ClickableObject>())
+                    hit = hit.parent;
+
+            if (hit.GetComponent<ClickableObject>())
+            {
+                Debug.Log("LClick: " + hit.name);
+
+                hit.GetComponent<ClickableObject>().LClick(transform.parent.parent.gameObject);
+            }
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Transform hit = castRay(10f);
+
+            if (!hit)
+                return;
+            if (hit.parent)
+                if (hit.parent.GetComponent<ClickableObject>())
+                    hit = hit.parent;
+
+            if (hit.GetComponent<ClickableObject>())
+            {
+                Debug.Log("RClick: " + hit.name);
+
+                hit.GetComponent<ClickableObject>().RClick(transform.parent.parent.gameObject);
+            }
+        }
+    }
+
+    public Transform castRay(float dist)
+    {
+        RaycastHit hit;
+        Ray ray = transform.parent.parent.FindChild("Camera").GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 15f))
+        {
+            return hit.transform;
+        }
+        else
+            return null;
+    }
 }

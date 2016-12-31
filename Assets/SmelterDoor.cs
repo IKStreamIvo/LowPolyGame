6bcc82fd621f7d8eb4d08c25c77ef2d433355a42
Scouter <markveenstra14@gmail.com>
2016-12-31 19:34:50 +0100

    Added - DoorAnimation Script

--- Assets/SmelterDoor.cs
+++ Assets/SmelterDoor.cs
@@ -6,24 +6,20 @@ public class SmelterDoor : ClickableObject {
 
     private bool open = false;
 
-    public override void LClick(GameObject source)
-    {
-        transform.parent.GetComponent<Animation>().Play("DoorOpen");
-    }
 
     public override void RClick(GameObject source)
     {
-        if (!open && !transform.parent.GetComponent<Animation>().isPlaying)
+        if (!open && !GetComponent<Animation>().isPlaying)
         {
-            transform.parent.GetComponent<Animation>()["DoorOpen"].speed = 1;
-            transform.parent.GetComponent<Animation>().Play("DoorOpen");
+            GetComponent<Animation>()["DoorOpen"].speed = 1;
+            GetComponent<Animation>().Play("DoorOpen");
             open = !open;
         }
-        else if (open && !transform.parent.GetComponent<Animation>().isPlaying)
+        else if (open && !GetComponent<Animation>().isPlaying)
         {
-            transform.parent.GetComponent<Animation>()["DoorOpen"].speed = -1;
-            transform.parent.GetComponent<Animation>()["DoorOpen"].time = transform.parent.GetComponent<Animation>()["DoorOpen"].length;
-            transform.parent.GetComponent<Animation>().Play("DoorOpen");
+            GetComponent<Animation>()["DoorOpen"].speed = -1;
+            GetComponent<Animation>()["DoorOpen"].time = GetComponent<Animation>()["DoorOpen"].length;
+            GetComponent<Animation>().Play("DoorOpen");
             open = !open;
         }
     }
